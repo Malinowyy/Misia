a46e7d5842c3fe8f2b2e158b78da6b60e14abc9e
Malinowyy <63302480+Malinowyy@users.noreply.github.com>
2026-02-04 19:36:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -54,16 +54,16 @@
 })();
 */
 const messages = [
-    "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "Na pewno?",
+    "Jak to ):",
+    "No weeeeź",
+    "Nina?",
+    "Ninaaaa.....",
+    "Jeśli to zrobisz będzie mi bardzo przykro ): ):",
+    "Ostatnia szansa, bo powiem misiuni...",
+    "A za jogurtową milke?",
+    "Okej, czyli nie...",
+    "NO ZGÓDŹ SIEEEEEEEE PROSZEEE"
 ];
 
 let messageIndex = 0;
